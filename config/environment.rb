0d16e1b63ccc10605fed24513b0472c6ef5891bc
bharti gurnaney <bharti0717@yahoo.com>
2016-11-07 16:36:00 +0000

    created environment.rb file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/best_cities/scraper'
+require_relative '../lib/best_cities/city'
+require_relative '../lib/best_cities/cli'
