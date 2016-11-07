fa1f3780ae79a2c28ed33c3cb4267f9b72b4a972
bharti gurnaney <bharti0717@yahoo.com>
2016-11-07 16:41:21 +0000

    created city.rb file

--- lib/best_cities/city.rb
+++ lib/best_cities/city.rb
@@ -1,1 +1,26 @@
+class BestCity::City
 
+  attr_accessor :population, :median_annual_salary,:quality_of_life ,:overall_value,:rank_city, :description
+
+  @@all = []
+
+  def initialize(rank_city=nil, population=nil, median_annual_salary=nil, quality_of_life=nil, overall_value=nil,description=nil)
+    @description = description
+    @rank_city = rank_city
+    @population=population
+    @median_annual_salary=median_annual_salary
+    @quality_of_life=quality_of_life
+    @overall_value=overall_value
+    @@all.unshift(self)
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(id)
+    self.all[id-1]
+  end
+
+  end
